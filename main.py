81d09909e970fd0525804643a2df806ff80f072e
orlandomarin <orlandomarin@orlandos-air.lan>
2024-06-04 20:16:36 -0400

    Fifth Python Program - Lists

---
+++ main.py
@@ -0,0 +1,16 @@
+# Assign the numbers 10, 11, 12, and 13 to a list called testScores
+# Print the numbers at index 3 and index -1
+
+# create a list called testscores
+test_scores = [10, 11, 12, 13]
+
+# variable indexThree contains index 3 from testScores List
+index_three = test_scores[3]
+
+# variable indexNegativeOne contains index -1 from testScores List
+index_negative_one = test_scores[-1]
+
+# print index 3 and index -1 from the testScores list
+print(f"Index 3 is {index_three}, Orlando")
+
+print(f"Index -1 is {index_negative_one}, Orlando")
